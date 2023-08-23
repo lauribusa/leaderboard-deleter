9a0f3afb97dea6795edb825c7a59a465d177180e
Glitch (steam-leaderboard-deleter) <none>
2023-08-23 16:34:48 +0000

    ⤵️↩️ Updated with Glitch

--- server.js
+++ server.js
@@ -41,63 +41,76 @@ if (seo.url === "glitch-default") {
 }
 
 fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
   let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
   return reply.view("/src/pages/index.hbs", params);
 });
 
 fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
   let params = { seo: seo };
 
+  const formEntries = request.body;
   const deleteConfirmation = request.body.delete;
 
-  let color = request.body.color;
-  const formEntries = request.body;
-  const nameEntry = formEntries.name.trim();
+  if (deleteConfirmation) {
+    const responseData = {};
+    let leaderboards = formEntries.leaderboards;
+    let appid = formEntries.appid;
+    let key = formEntries.key;
 
-  if (!nameEntry) {
-    params.error = "Search input is missing or invalid.";
-    return reply.view("/src/pages/index.hbs", params);
+    DeleteLeaderboards(appid, key, leaderboards)
+      .then((result) => {
+        console.log({ axiosAllResponse: result });
+        result.map((value) => {
+          if (value.status != 200) {
+            params.error = value.statusText;
+          } else {
+            params.success = value.statusText;
+          }
+          params.data = value.data;
+        });
+      })
+      .catch((err) => {
+        console.log({ afterDeleteError: err });
+        params.error = err;
+      })
+      .finally((res) => {
+        console.log({ params });
+        return reply.view("/src/pages/index.hbs", params);
+      });
+  } else {
+    let color = request.body.color;
+    const nameEntry = formEntries.name.trim();
+
+    if (!nameEntry) {
+      params.error = "Search input is missing or invalid.";
+      return reply.view("/src/pages/index.hbs", params);
+    }
+
+    getLeaderboards(formEntries.appid, formEntries.key).then((response) => {
+      if (response.error) {
+        console.log({ axiosGETError: response.error });
+        const cleanedText = response.error.data.replace(/<\/?[^>]+(>|$)/g, "");
+        params.error = cleanedText;
+        return reply.view("/src/pages/index.hbs", params);
+      }
+      const data = response.response.data.response.leaderboards;
+      const filteredLeaderboards = FilterLeaderboards(data, nameEntry);
+      if (filteredLeaderboards.length <= 0) {
+        params.error = "Search input returned no results.";
+        return reply.view("/src/pages/index.hbs", params);
+      }
+      params.leaderboards = filteredLeaderboards;
+      params.form = {
+        appid: formEntries.appid,
+        key: formEntries.key,
+        name: formEntries.name,
+      };
+      
+      return reply.view("/src/pages/index.hbs", params);
+    });
   }
-
-  getLeaderboards(formEntries.appid, formEntries.key).then((response) => {
-    if (response.error) {
-      console.log({ err: response.error });
-      const cleanedText = response.error.data.replace(/<\/?[^>]+(>|$)/g, "");
-      params.error = cleanedText;
-      return reply.view("/src/pages/index.hbs", params);
-    }
-    const data = response.response.data.response.leaderboards;
-    const filteredLeaderboards = FilterLeaderboards(data, nameEntry);
-    if(filteredLeaderboards.length <= 0){
-      params.error = "Search input returned no results.";
-      return reply.view("/src/pages/index.hbs", params);
-    }
-    params.leaderboards = filteredLeaderboards;
-    console.log({LB: params.leaderboards});
-    return reply.view("/src/pages/index.hbs", params);
-  });
 });
 
-// Run the server and report out to the logs
 fastify.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
   function (err, address) {
@@ -122,32 +135,31 @@ async function getLeaderboards(appid, key) {
   return data;
 }
 
-async function deleteLeaderboard(appid = "", key = "", name = "") {
-  if (!appid || !key || !name) return;
-  var formdata = new FormData();
-  formdata.append("key", "B4C158613225C0FD6B1BA6F138FC6D32");
-  formdata.append("appid", "1968730");
-  formdata.append("name", name ? name : "z_duo_2022.10.1_beta");
-
-  var requestOptions = {
-    method: "POST",
-    body: formdata,
-    redirect: "follow",
-  };
-
+async function DeleteLeaderboards(appid, key, names) {
   const url = `${steamworksUrl}${deleteLeaderboardUrl}`;
+  const data = {};
 
-  const response = await fetch(
-    `${url}?appid=${appid}&key=${key}&name=${name}`,
-    requestOptions
-  );
-  return response.json();
+  let axiosRequests = [];
+  let namesArray = names.split(",").filter((str) => String(str).trim());
+
+  namesArray.map((name) => {
+    const request = axios.post(
+      `${url}`,
+      `appid=${appid}&key=${key}&name=${name}`
+    );
+    axiosRequests.push(request);
+  });
+  const response = {};
+  const res = await axios.all(axiosRequests);
+  return res;
 }
 
-function FilterLeaderboards(leaderboards, searchInput){
+function FilterLeaderboards(leaderboards, searchInput) {
   const regex = new RegExp(`${searchInput}`, "i");
-  
-  const filteredLeaderboards = leaderboards.filter(leaderboard => regex.test(leaderboard.name));
-  
+
+  const filteredLeaderboards = leaderboards.filter((leaderboard) =>
+    regex.test(leaderboard.name)
+  );
+
   return filteredLeaderboards;
 }
